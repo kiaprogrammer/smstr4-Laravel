13a0b0212bcaa0cca19bb1d9341d10eef15cfd9c
kiaprogrammer <kia.man2993@gmail.com>
2025-02-24 10:52:07 +0700

    Tugas 3 & 4 Route dan Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\ManagementUserController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::get('/foo', function(){
+//     return 'Hello World';
+// });
+
+// Route::get('/foo/{id}', function($id){
+//     return 'User ='.$id;
+// });
+
+// Route::get('/user', 'UserController@controller');
+
+// Route::redirect('/coba', '/sini');
+
+// Route::view('/welcome','welcome');
+
+// Route::get('/userss/{name?}', function($name=null){
+//     return $name;
+// });
+
+// Route::get('/users/{nama?}', function($nama = 'John'){
+//     return $nama;
+// });
+
+// Route::get('/user1', [ManagementUserController::class, 'index']);
+
+Route::resource('/user1', ManagementUserController::class);
+
+Route::get('/home', function(){
+    return view('home');
 });
