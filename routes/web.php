3532401db5dc498a48f26ef70b26af092073c457
Azkia <kia.man2993@gmail.com>
2025-03-17 13:18:51 +0700

    Acara 8 Menambahkan NICE ADMIN

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Backend\DashboardController;
 use App\Http\Controllers\ManagementUserController;
 use App\Http\Controllers\UserController;
 
 Route::get('/home', [HomeController::class,'index'])->name('home');
+
+Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
 
 Route::get('/user/{name}', [UserController::class, 'show']);
 
