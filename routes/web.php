95711e9ec8645a9c357f0a9da2f9b70c32133564
Azkia <kia.man2993@gmail.com>
2025-03-17 11:17:22 +0700

    Menambahkan boostrap frontend dari Acara 7 BKPM

--- routes/web.php
+++ routes/web.php
@@ -1,77 +1,14 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\ManagementUserController;
 use App\Http\Controllers\UserController;
 
-Route::get('/home', [HomeController::class,'index']);
-Route::resource('users',ManagementUserController::class);
+Route::get('/home', [HomeController::class,'index'])->name('home');
 
-Route::get('/user/{name}',[UserController::class,'show']);
+Route::get('/user/{name}', [UserController::class, 'show']);
 
-// Route::prefix('admin')->group(function(){
-//     Route::get('/dashboard', function(){
-//         return "Halaman Admin Dashboard";
-//     });
-//     Route::get('/profile',function(){
-//         return "Halaman Admin Profile";
-//     });
-// });
+Route::get('/user', [UserController::class, 'controller']);
 
-// Route::middleware(['auth'])->group(function(){
-//     Route::get('/dashboard', function(){
-//         return 'Ini dashboard untuk pengguna yang sudah login';
-//     });
-// });
-
-// Route::get('/home',[ManagementUserController::class,'index']);
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/hello', function () {
-//     return "Hallo, selamat datang di Laravel!";
-// });
-
-// Route::get('/user/{name}',function($name){
-//     return "Halloo, $name";
-// });
-
-// Route::get('/user/{name?}',function ($name = 'Tamu'){
-//     return "Halo, $name!";
-// });
-
-// Route::get('/foo', function(){
-//     return 'Hello World';
-// });
-
-// Route::get('/foo/{id}', function($id){
-//     return 'User ='.$id;
-// });
-
-// Route::get('/user', 'UserController@controller');
-
-// Route::redirect('/coba', '/sini');
-
-// Route::view('/welcome','welcome');
-
-// Route::get('/userss/{name?}', function($name=null){
-//     return $name;
-// });
-
-// Route::get('/users/{nama?}', function($nama = 'John'){
-//     return $nama;
-// });
-
-// Route::get('/user1', [ManagementUserController::class, 'index']);
-
-// Route::resource('/user1', ManagementUserController::class);
-
-// Route::get('/home', function(){
-//     return view('home');
-// });
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/user1', [ManagementUserController::class, 'index']);
